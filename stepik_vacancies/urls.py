91188b874ecbedf49c6fae81799f2487699e81ca
VadimKST <vadstge@gmail.com>
2021-03-28 10:27:44 +0300

    Second Init

--- stepik_vacancies/urls.py
+++ stepik_vacancies/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-import vacancy.views as vacancy_views
-from vacancy.views import custom_handler404, custom_handler500
+from vacancy.views import custom_handler404, custom_handler500, HomeView, VacancyView
+
 
 handler404 = custom_handler404
 handler500 = custom_handler500
 
 urlpatterns = [
-    path('', vacancy_views.Home, name='home'),
-    path('vacancies/', vacancy_views.Vacancies, name='vacancies'),
-    path('vacancies/cat/backend', vacancy_views.Specialty, name='specialty'),
-    path('companies/345', vacancy_views.Company, name='company'),
-    path('vacancies/22', vacancy_views.Vacancy, name='vacancy')
+    path('', HomeView.as_view(), name='home'),
+    # path('vacancies/', vacancy_views.Vacancies, name='vacancies'),
+    # path('vacancies/cat/backend', vacancy_views.Specialty, name='specialty'),
+    # path('companies/<int:comp_id>', vacancy_views.Company, name='company'),
+    path('vacancies/22', VacancyView.as_view(), name='vacancy')
 ]
--- vacancy/views.py
+++ vacancy/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError, Http404
-from django.views.generic import ListView
+
+from django.http import HttpResponseNotFound, HttpResponseServerError, Http404
+from django.views.generic import ListView, TemplateView
+from vacancy.models import Vacancy, Company, Specialty
 
 
 def custom_handler500(request):
@@ -11,21 +12,60 @@ def custom_handler404(request, exception):
     return HttpResponseNotFound('Страница не найдена!')
 
 
-def Home(request):
-    return render(request, 'vacancy/index.html')
+class HomeView(TemplateView):
+    template_name = 'vacancy/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Джуманджи'
+        context['specialties'] = Specialty.objects.all()
+        return context
+
+    def get_queryset(self):
+        return Company.objects.value()
+#
+#
+# class Vacancies(ListView):
+#     model = Vacancy
+#     template_name = 'vacancy/vacancies.html'
+#     context_object_name = 'Vacancy'
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['page_title'] = 'Джуманджи'
 
 
-def Vacancies(request):
-    return render(request, 'vacancy/vacancies.html')
+# class Specialty(ListView):
+#     model = Specialty
+#     template_name = 'vacancy/index.html'
+#     context_object_name = 'Specialty'
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['page_title'] = 'Джуманджи'
 
 
-def Specialty(request):
-    return render(request, 'vacancy/vacancies.html')
+# class Company(ListView):
+#     model = Company
+#     template_name = 'vacancy/index.html'
+#     context_object_name = 'Company'
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['page_title'] = 'Джуманджи'
 
 
-def Company(request):
-    return render(request, 'vacancy/company.html')
+class VacancyView(ListView):
+    model = Vacancy
+    template_name = 'vacancy/vacancy.html'
+    context_object_name = 'Vacancy'
 
-
-def Vacancy(request):
-    return render(request, 'vacancy/vacancy.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Вакансия | Джуманджи'
+        context['Vacancy'] = Vacancy.objects.get(pk=1)
+        print(context)
+        return context
+    #
+    # def get_queryset(self):
+    #     return Vacancy.objects.filter(pk=self.kwargs['vac_id']).select_related('company')
